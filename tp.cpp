7ae1df5f975b664c89badbbdfdb62e9aa1edbbdb
nauel2015 <nricheri@frba.utn.edu.ar>
2025-02-08 16:39:48 -0300

    Update tp.cpp

--- tp.cpp
+++ tp.cpp
@@ -1,8 +1,86 @@
-#include <iostream>
+
+// clase sobre el trabajo practico de la escuela de verano algoritmos.
+
+#include<iostream>
+#include<stdlib.h>
+#include<string.h>
+
 using namespace std;
 
-int main(){
+// 1- Declarar el campo de la informacion del Nodo.
 
+struct tipoInfo
+{
+    int ID_Accesorio;
+    int cantidad;
+};
 
-  return 0;
+// 2- Declarar struct Nodo()
+
+struct Nodo
+{
+    tipoInfo info;
+    Nodo* sgte;
+};
+
+// 3- Declarar struct registro de modelos.
+
+struct registroModelos
+{
+    int ID_modelo;
+    char descripcion[100+1];
+    float precio_base;
+    char temporada;
+    Nodo* ListaDeCompon = NULL;
+};
+
+// 4- Declarar vector de modelos.
+
+registroModelos vectorModelos[50];
+
+// Los puntos 1 al 4 son los necesarios para declarar las estructuras del primero de los vectores.
+
+// 5- Declarar struct tipoInfo de proveedores.
+
+struct tipoInfoProveedores
+{
+    int ID;
+    char nombre[50];
+    float valor_unitario;
+};
+
+// 6- Declarar struct Nodo de proveedores.
+
+struct NodoProveedores
+{
+    tipoInfoProveedores info;
+    NodoProveedores* sgte;
+};
+
+struct componentes 
+{
+    int ID;
+    char descripcion[100+1];
+    NodoProveedores* listaProveedores;
+    int stock;
+};
+
+componentes vectorComponentes[1000];
+
+struct pedido
+{
+    int ID_Pedido;
+    int ID_Linea;
+    int ID_Modelo;
+    int Cantidad;
+    float costo;
+};
+
+// Resolucion estrategica del problema:
+// 1- Abrir archivo
+
+int main()
+{
+    system("PAUSE");
+    return 0;
 }
