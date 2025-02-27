549a070b14b8985b5487b7774748f7b7cb91b21f
kevirui <kevinagustinrockz@gmail.com>
2025-02-27 16:40:50 -0300

    Update tp

--- tp.cpp
+++ tp.cpp
@@ -7,26 +7,62 @@
 
 using namespace std;
 
-struct Proveedor
+struct Proveedor // InfoP
 { // Estructura para almacenar los proveedores de un componente
     int id;
     char nombre[101];
     float valor_unitario;
 };
 
-struct Componente
+struct NodoProveedor{ // NodoP
+	Proveedor info;
+	NodoProveedor* sgte;
+};
+
+struct ListaProveedores{
+  NodoProveedor* primero;
+  ListaProveedores(){
+    primero = nullptr;
+  }
+};
+
+struct Componente // InfoC
 { // Estructura para almacenar los componentes de un modelo
     int id;
     char descripcion[101];
-    Proveedor proveedores[3];
-    int num_proveedores;
     int stock;
+    ListaProveedores* lista_proveedores;
 };
 
-struct ComponenteModelo
+struct NodoComponentes{ // NodoC
+	Componente info;
+	NodoComponentes* sgte;
+};
+
+struct ListaComponentes{
+	NodoComponentes* primero;
+	ListaComponentes(){
+	  primero = nullptr;
+	}
+};
+
+struct ComponenteModelo // InfoCM
 { // Estructura para almacenar los componentes de un modelo
     int id_accesorio;
     int cantidad;
+};
+
+struct NodoComponenteModelo // NodoCM
+{
+	ComponenteModelo info;
+	NodoComponenteModelo* sgte;
+};
+
+struct ListaComponentesModelo{
+  NodoComponenteModelo* primero;
+  ListaComponentesModelo(){
+    primero = nullptr;
+  }
 };
 
 struct Modelo
@@ -35,8 +71,19 @@ struct Modelo
     char descripcion[101];
     float precio_base;
     char temporada[10];
-    ComponenteModelo componentes[5];
-    int num_componentes;
+    ListaComponentesModelo* componentes;
+};
+
+struct NodoModelo{
+  Modelo info;
+  NodoModelo* sgte;
+};
+
+struct ListaModelos{
+  NodoModelo* primero;
+  ListaModelos(){
+    primero = nullptr;
+  }
 };
 
 struct Pedido
@@ -69,39 +116,124 @@ string nombreMod[] = { // Descripciones de los modelos
 
 string nombresProv[] = {"Proveedor A", "Proveedor B", "Proveedor C"}; // Nombres de los proveedores
 
-Modelo modelos[50];
-Componente componentes[1000];
-
 int num_modelos = 50, num_componentes = 20; // Cantidad de modelos y componentes
+
+// Variables globales para las listas 
+ListaComponentes* lista_componentes;
+ListaModelos* lista_modelos;
+					
+void agregar_proveedor(int id, const char* nombre, float valor, ListaProveedores* lista){
+  NodoProveedor* nuevo = new NodoProveedor;
+	nuevo->info.id = id;
+	strcpy(nuevo->info.nombre, nombre);
+	nuevo->info.valor_unitario= valor;
+	nuevo->sgte = nullptr;
+
+	// Insertar al final
+	if(lista->primero == nullptr){
+		lista->primero = nuevo;
+	}else{
+	  NodoProveedor* actual = lista->primero;
+	  while(actual->sgte != nullptr){
+	    actual = actual->sgte;
+	  }
+	  actual->sgte = nuevo;
+	}
+}
+
+void agregar_componente(int id, const char* descripcion, int stock){
+  NodoComponentes* nuevo = new NodoComponentes;
+	nuevo->info.id = id+1000; 
+	strcpy(nuevo->info.descripcion, descripcion);
+	nuevo->info.stock = stock;
+	nuevo->info.lista_proveedores = new ListaProveedores();
+	nuevo->sgte = nullptr;
+
+	// Insertar al final
+	if(lista_componentes->primero == nullptr){
+		lista_componentes->primero = nuevo;
+	}else{
+	  NodoComponentes* actual = lista_componentes->primero;
+	  while(actual->sgte != nullptr){
+	    actual = actual->sgte;
+	  }
+	  actual->sgte = nuevo;
+	}
+}
+
+void agregar_componente_modelo(int id_accesorio, int cantidad, ListaComponentesModelo* lista){
+  NodoComponenteModelo* nuevo = new NodoComponenteModelo;
+	nuevo->info.id_accesorio = id_accesorio + 1000;
+	nuevo->info.cantidad = cantidad; 
+	nuevo->sgte = nullptr;
+
+	// Insertar al final
+	if(lista->primero == nullptr){
+		lista->primero = nuevo;
+	}else{
+	  NodoComponenteModelo* actual = lista->primero;
+	  while(actual->sgte != nullptr){
+	    actual = actual->sgte;
+	  }
+	  actual->sgte = nuevo;
+	}
+}
+
+void agregar_modelo(int id, const char* descripcion, float precio_base, const char* temporada){
+  NodoModelo* nuevo = new NodoModelo;
+	nuevo->info.id = id;
+	strcpy(nuevo->info.descripcion, descripcion);
+	nuevo->info.precio_base = precio_base;
+	strcpy(nuevo->info.temporada, temporada);
+	nuevo->info.componentes = new ListaComponentesModelo();
+	nuevo->sgte = nullptr;
+
+	// Insertar al final
+	if(lista_modelos->primero == nullptr){
+		lista_modelos->primero = nuevo;
+	}else{
+	  NodoModelo* actual = lista_modelos->primero;
+	  while(actual->sgte != nullptr){
+	    actual = actual->sgte;
+	  }
+	  actual->sgte = nuevo;
+	}
+}
 
 void inicializar_datos()
 {                      // Inicializa los datos de los componentes y modelos
+    lista_componentes = new ListaComponentes();
+    lista_modelos = new ListaModelos();
+    
     srand(time(NULL)); // Inicializa el generador de números aleatorios
+    
     for (int i = 0; i < num_componentes; i++)
     {                          // Inicializa los componentes
-        componentes[i].id = i; // Asigna un ID de componente
-        strcpy(componentes[i].descripcion, nombreComp[i].c_str());
-        componentes[i].num_proveedores = 3;         // Cantidad de proveedores por componente
-        componentes[i].stock = 50 + (rand() % 500); // Stock aleatorio entre 50 y 550
+      agregar_componente(i, nombreComp[i].c_str(), 50 + (rand() % 500)); // Agrega un componente
+
+      // Obtener el componente recien agregado
+      NodoComponentes* comp = lista_componentes->primero;
+      while(comp->sgte != nullptr){
+	comp = comp->sgte;
+      }
         for (int j = 0; j < 3; j++)
         {                                                                         // Inicializa los proveedores de cada componente
-            componentes[i].proveedores[j].id = j;                                 // Asigna un ID de proveedor
-            strcpy(componentes[i].proveedores[j].nombre, nombresProv[j].c_str()); // Asigna un nombre de proveedor
-            componentes[i].proveedores[j].valor_unitario = 10.0 + (rand() % 100); // Precio aleatorio entre 10 y 110
+	  agregar_proveedor(j, nombresProv[j].c_str(), 10.0 + (rand() % 100), comp->info.lista_proveedores); // Agrega un proveedor
         }
     }
 
     for (int i = 0; i < num_modelos; i++)
     {                                                                       // Inicializa los modelos
-        modelos[i].id = i;                                                  // Asigna un ID de modelo
-        strcpy(modelos[i].descripcion, nombreMod[i].c_str());               // Asigna una descripción de modelo
-        modelos[i].precio_base = 50.0 + (rand() % 200);                     // Precio base aleatorio entre 50 y 250
-        strcpy(modelos[i].temporada, (i % 2 == 0) ? "Verano" : "Invierno"); // Asigna una temporada de modelo
-        modelos[i].num_componentes = 3 + (rand() % 3);                      // Cantidad de componentes por modelo
-        for (int j = 0; j < modelos[i].num_componentes; j++)
+      agregar_modelo(i, nombreMod[i].c_str(), 50.0 + (rand() % 200), (i%2==0) ? "Verano" : "Invierno"); // Agrega un modelo
+      // Obtener el modelo recien agregado
+      NodoModelo* mod = lista_modelos->primero;
+      while(mod->sgte != nullptr){
+	mod = mod->sgte;
+      }
+      int num_comp = 3 + (rand() % 3);
+        for (int j = 0; j < num_comp; j++)
         {                                                                      // Inicializa los componentes de cada modelo
-            modelos[i].componentes[j].id_accesorio = rand() % num_componentes; // Asigna un ID de componente
-            modelos[i].componentes[j].cantidad = 1 + (rand() % 10);            // Cantidad aleatoria entre 1 y 10
+	  agregar_componente_modelo(rand() % num_componentes, 1 + (rand() % 10), mod->info.componentes); // Agrega un componente al modelo
         }
     }
 }
@@ -109,19 +241,56 @@ void inicializar_datos()
 void mostrar_componentes()
 {                                                         // Muestra la información de los componentes
     cout << "=== Información de Componentes ===" << endl; // Muestra la información de los componentes
-    for (int i = 0; i < num_componentes; i++)
-    { // Recorre los componentes
+    NodoComponentes* comp = lista_componentes->primero;
+    while(comp != nullptr){
         cout << endl;
-        cout << "ID: " << componentes[i].id << endl;                  // Muestra el ID del componente
-        cout << "Componente: " << componentes[i].descripcion << endl; // Muestra la descripción del componente
-        cout << "Stock: " << componentes[i].stock << endl;            // Muestra el stock del componente
+        cout << "ID: " << comp->info.id << endl;                  // Muestra el ID del componente
+        cout << "Componente: " << comp->info.descripcion << endl; // Muestra la descripción del componente
+        cout << "Stock: " << comp->info.stock << endl;            // Muestra el stock del componente
         cout << "Proveedores:" << endl;                               // Muestra los proveedores del componente
-        for (int j = 0; j < componentes[i].num_proveedores; j++)
+	NodoProveedor* prov = comp->info.lista_proveedores->primero;
+        while(prov!=NULL)
         { // Recorre los proveedores del componente
-            double valor = componentes[i].proveedores[j].valor_unitario;
-            cout << " - " << componentes[i].proveedores[j].nombre << " ( Valor: $" << (int)(valor * 100 + 0.5) / 100.0 << " )" << endl; // Muestra datos del proveedor
+            double valor = prov->info.valor_unitario;
+            cout << " - " << prov->info.nombre << " ( Valor: $" << (int)(valor * 100 + 0.5) / 100.0 << " )" << endl; // Muestra datos del proveedor
+	    prov = prov->sgte;
         }
+	comp = comp->sgte;
     }
+}
+
+void liberar_memoria(){
+  // Liberar memoria de lista componentes
+  NodoComponentes* comp = lista_componentes->primero;
+  while(comp != nullptr){
+    NodoProveedor* prov = comp->info.lista_proveedores->primero;
+    while(prov != NULL){
+      NodoProveedor* aux = prov;
+      prov = prov->sgte;
+      delete aux;
+    }
+    delete comp->info.lista_proveedores;
+    NodoComponentes* aux = comp;
+    comp = comp->sgte;
+    delete aux;
+  }
+  delete lista_componentes;
+
+  // Liberar memoria de lista modelos
+  NodoModelo* mod = lista_modelos->primero;
+  while(mod != nullptr){
+    NodoComponenteModelo* comp_mod = mod->info.componentes->primero;
+    while(comp_mod != NULL){
+      NodoComponenteModelo* aux = comp_mod;
+      comp_mod = comp_mod->sgte;
+      delete aux;
+    }
+    delete mod->info.componentes;
+    NodoModelo* aux = mod;
+    mod = mod->sgte;
+    delete aux;
+  }
+  delete lista_modelos;
 }
 
 void generar_pedidos(const char *archivo_pedidos)
@@ -157,60 +326,80 @@ void procesar_pedidos(const char *archivo_entrada, const char *archivo_salida)
     float costo_total_pedidos = 0; // Costo total de los pedidos
     while (fread(&pedido, sizeof(Pedido), 1, archivo_in) == 1)
     {                                                                       // Lee los pedidos del archivo
-        int id_mod = pedido.id_modelo;                                      // ID del modelo del pedido
-        float costo_pedido = modelos[id_mod].precio_base * pedido.cantidad; // Costo base del pedido
+      NodoModelo* mod = lista_modelos->primero;
+      while(mod!=NULL && mod->info.id != pedido.id_modelo){
+	mod = mod->sgte;
+      }
+      if(mod ==NULL){
+	cout<< "Modelo no encontrado"<< endl;
+	continue;
+      }
+
+        float costo_pedido = mod->info.precio_base * pedido.cantidad; // Costo base del pedido
 
         cout << endl;
         cout << "=== Pedido ID: " << pedido.id_pedido << " ===" << endl; // Muestra el ID del pedido
-        cout << "Modelo: " << modelos[id_mod].descripcion << endl;       // Muestra la descripción del modelo
+        cout << "Modelo: " << mod->info.descripcion << endl;       // Muestra la descripción del modelo
         cout << "Cantidad: " << pedido.cantidad << endl;                 // Muestra la cantidad del pedido
-        double precioBase = (int)(modelos[id_mod].precio_base * 100 + 0.5) / 100.0;
+        double precioBase = (int)(mod->info.precio_base * 100 + 0.5) / 100.0;
         cout << "Precio Base: $" << precioBase << endl;             // Muestra el precio base del modelo
-        cout << "Temporada: " << modelos[id_mod].temporada << endl; // Muestra la temporada del modelo
+        cout << "Temporada: " << mod->info.temporada << endl; // Muestra la temporada del modelo
         cout << endl;
         cout << "Componentes necesarios:" << endl; // Muestra los componentes necesarios
 
-        for (int j = 0; j < modelos[id_mod].num_componentes; j++)
-        {                                                                                       // Recorre los componentes del modelo
-            int id_comp = modelos[id_mod].componentes[j].id_accesorio;                          // ID del componente
-            int cantidad_necesaria = modelos[id_mod].componentes[j].cantidad * pedido.cantidad; // Cantidad necesaria del componente
+	NodoComponenteModelo* comp_mod = mod->info.componentes->primero;
+	while(comp_mod != NULL){
+	  NodoComponentes* comp = lista_componentes->primero;
+	  while(comp != NULL && comp->info.id != comp_mod->info.id_accesorio){
+	    comp = comp->sgte;
+	  }
+	  if(comp==NULL){
+	    cout<< "Componente no encontrado"<< endl;
+	    comp_mod = comp_mod->sgte;
+	    continue;
+	  }
+            int cantidad_necesaria = comp_mod->info.cantidad * pedido.cantidad; // Cantidad necesaria del componente
 
             cout << endl;
-            cout << "ID: " << componentes[id_comp].id + 1 << endl;              // Muestra el ID del componente
-            cout << "Componente: " << componentes[id_comp].descripcion << endl; // Muestra la descripción del componente
-            cout << "Stock actual: " << componentes[id_comp].stock << endl;     // Muestra el stock del componente
+            cout << "ID: " << comp->info.id + 1 << endl;              // Muestra el ID del componente
+            cout << "Componente: " << comp->info.descripcion<< endl; // Muestra la descripción del componente
+            cout << "Stock actual: " << comp->info.stock << endl;     // Muestra el stock del componente
             cout << "Cantidad necesaria: " << cantidad_necesaria << endl;       // Muestra la cantidad necesaria del componente
             cout << "Proveedores:" << endl;                                     // Muestra los proveedores del componente
-
             float menor_precio = FLT_MAX;                                  // Precio del proveedor más barato
-            int proveedor_elegido = -1;                                    // ID del proveedor elegido
-            for (int p = 0; p < componentes[id_comp].num_proveedores; p++) // Recorre los proveedores del componente
+            NodoProveedor* proveedor_elegido = NULL;                                    // ID del proveedor elegido
+	    NodoProveedor* prov = comp->info.lista_proveedores->primero;
+	    while(prov !=NULL)
             {
-                double valor = componentes[id_comp].proveedores[p].valor_unitario;
+                double valor = prov->info.valor_unitario;
                 cout
-                    << " - " << componentes[id_comp].proveedores[p].nombre                // Muestra el nombre del proveedor
+                    << " - " << prov->info.nombre // Muestra el nombre del proveedor
                     << " ( Valor: $" << (int)(valor * 100 + 0.5) / 100.0 << " )" << endl; // Muestra el precio del proveedor
 
-                if (componentes[id_comp].proveedores[p].valor_unitario < menor_precio)
+                if (prov->info.valor_unitario < menor_precio)
                 {                                                                      // Si el precio es menor al precio mínimo
-                    menor_precio = componentes[id_comp].proveedores[p].valor_unitario; // Actualiza el precio mínimo
-                    proveedor_elegido = p;                                             // Actualiza el proveedor elegido
+                    menor_precio = prov->info.valor_unitario; // Actualiza el precio mínimo
+                    proveedor_elegido = prov;                                             // Actualiza el proveedor elegido
                 }
+		prov = prov->sgte;
             }
 
-            if (componentes[id_comp].stock < cantidad_necesaria)
+            if (comp->info.stock < cantidad_necesaria)
             {                                                                                   // Si no hay stock suficiente
-                cout << "Error: Stock insuficiente para el componente " << id_comp + 1 << endl; // Muestra un mensaje de error
+                cout << "Error: Stock insuficiente para el componente " << comp->info.id + 1 << endl; // Muestra un mensaje de error
+		comp_mod = comp_mod->sgte;
                 continue;                                                                       // Continúa con el siguiente componente
             }
 
-            componentes[id_comp].stock -= cantidad_necesaria;           // Actualiza el stock del componente
+            comp->info.stock -= cantidad_necesaria;           // Actualiza el stock del componente
             float costo_componente = menor_precio * cantidad_necesaria; // Costo del componente
             costo_pedido += costo_componente;                           // Actualiza el costo del pedido
 
-            cout << "Proveedor elegido: " << componentes[id_comp].proveedores[proveedor_elegido].nombre << endl; // Muestra el proveedor elegido
+            cout << "Proveedor elegido: " << proveedor_elegido->info.nombre << endl; // Muestra el proveedor elegido
             costo_componente = (int)(costo_componente * 100 + 0.5) / 100.0;
             cout << "Costo del componente: $" << costo_componente << endl; // Muestra el costo del componente
+
+	    comp_mod = comp_mod->sgte;
         }
 
         pedido.costo = costo_pedido;                     // Actualiza el costo del pedido
@@ -243,7 +432,9 @@ void procesar_pedidos(const char *archivo_entrada, const char *archivo_salida)
 int main()
 {                                                                // Función principal
     inicializar_datos();                                         // Inicializa los datos de los componentes y modelos
+    mostrar_componentes();                                       // Muestra la información de los componentes
     generar_pedidos("pedidos.dat");                              // Genera pedidos de ejemplo
     procesar_pedidos("pedidos.dat", "pedidos_actualizados.dat"); // Procesa los pedidos
+    liberar_memoria(); // Libera la memoria de los componentes y modelos
     return 0;
 }
